f08ca42245600c3e7068c2985c62bd897079ccb4
Stephen Lane <sophstep2015@gmail.com>
2021-10-09 20:55:51 -0500

    final

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -20,6 +20,7 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
     
+    #Append data into list 
     for row in csvreader:
         candid.append(str(row[2]))
 
